d73089b04581df9e28582b10c14489f426b1f5d8
EtaiWil <etaiwil2000@gmail.com>
2025-06-18 17:14:37 +0300

    fixed links to github and linkedin and finished under 900 px

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -10,26 +10,26 @@ export function Hero() {
           <h1 className="heading-primary">
             Hi! I'm <span className="bold">Etai Wilentzik </span> Software Developer 
           </h1>
-          <p className="hero-description">
-            I am a software developer with a passion for creating innovative solutions. 
-            I hold a BSc in Computer Science and have experience with various technologies
-            
-          </p>
+    <p className="hero-description">
+  I am a software developer with a passion for creating innovative solutions. 
+  I hold a BSc in Computer Science and have experience with various technologies.
+</p>
+
           <p className="hero-description">
             I'm a hard worker who enjoys working on challenging projects, collaborating with others, and  always eager to learn new skills.
           </p>
-          <a href="#cta" className="btn btn--full margin-right-sm center-text">
+          <a href="https://github.com/EtaiWilentzik" className="btn btn--full margin-right-sm center-text">
              github  
             <FaGithub />
           </a>
-          <a href="#how" className="btn btn--outline">
+          <a href="https://www.linkedin.com/in/etai-wilentzik/" className="btn btn--outline">
            linkedin 
             <FaLinkedin />
           </a>
 
         </div>
 
-        <div >
+        <div className="hero-img-box">
           <img src="image.jpg" className="hero-img" alt="me" />
         </div>
       </div>
